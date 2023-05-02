d83f6f6b6136d5100e5520176f531b9b34eeeb1c
Cecilia Torreira <celia.umbreon@gmail.com>
2023-05-02 15:31:44 +0100

    Segundo commit

--- app.js
+++ app.js
@@ -7,13 +7,21 @@ app.get('/', (req, res) => {
 })
 
 app.get("/home",(req,res)=> {
-  res.sendFile(__dirname+"/views/home.html")
+  res.sendFile(__dirname + "/views/home.html")
+})
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html")
 })
 
 
+app.get("/gallery", (req, res) =>{
+  res.sendFile(__dirname + "/views/gallery.html")
+})
 
-
-
+app.get("/works", (req, res) =>{
+res.sendFile(__dirname + "/views/works.html")
+})
 
 
 
